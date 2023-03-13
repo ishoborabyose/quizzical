2e208eda56a72c41a9753382b767463fabc74a45
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2023-03-13 18:29:41 +0200

    Added Quizical app

---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+const Menu = ({ start }) => {
+  return (
+    <div className="flex items-center justify-center flex-col gap-2">
+      <h1 className="text-[#293264] text-4xl">Quizzical</h1>
+      <span className="text-gray-600">Description</span>
+      <button
+        className="text-white rounded-[10px] py-4 px-8 bg-[#4d5b9e]"
+        onClick={() => start()}
+      >
+        Start Quizzical
+      </button>
+    </div>
+  );
+};
+
+export default Menu;
---
+++ src/components/Question.jsx
@@ -0,0 +1,48 @@
+import { nanoid } from "nanoid";
+import "../App.css";
+
+const Question = ({ q, id, handleClickAnswer }) => {
+  let result = q.answers;
+  const handleClick = (answer) => {
+    if (q.checked) {
+      return;
+    }
+    handleClickAnswer(id, answer);
+  };
+
+  return (
+    <div className="mb-3 w-full text-left">
+      <h3 className="text-base leading-[19px] font-bold mb-2  text-[#293264]">
+        {atob(q.question)}
+      </h3>
+      {result.map((answer) => {
+        let id = null;
+        if (q.checked) {
+          if (q.correct === answer) {
+            id = "correct";
+          } else if (q.selected === answer) {
+            id = "incorrect";
+          } else {
+            id = "not-selected";
+          }
+        }
+        return (
+          <button
+            key={nanoid()}
+            id={id}
+            className={`text-[#293264] bg-transparent border border-[#4d5b9e] font-medium  text-xs rounded-[10px] mr-[13px] mb-1 py-[2px] px-[8px] answer ${
+              answer === q.selected ? "bg-[#D6DBF5] selected" : ""
+            }  `}
+            onClick={() => handleClick(answer)}
+          >
+            {atob(answer)}
+          </button>
+        );
+      })}
+
+      <div className="mt-2 w-full border-t border-t-[#c4c8e8]"></div>
+    </div>
+  );
+};
+
+export default Question;
